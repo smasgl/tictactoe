e88749907da16c1e5e4da1c86af2d740c63e4244
smasgl <57876420+smasgl@users.noreply.github.com>
2020-07-15 07:49:25 +0200

    Update Program.cs

--- TicTacToeConsole/Program.cs
+++ TicTacToeConsole/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using TicTacToeGame;
 using Toolbox.ConsoleTools;
-using ConsoleHelper;
 using System.Linq;
 
 namespace TicTacToeConsole
@@ -12,35 +11,23 @@ namespace TicTacToeConsole
         static Player _player1;
         static Player _player2;
         static Game _game;
-        static List<Player> _knownPlayers;
+        static List<Player> _knownPlayers = new List<Player>();
 
-        static void Main(string[] args)
+        static void Main()
         {
-            #region Nickname Input
-
-            _knownPlayers = new List<Player>();
-
-            _player1 = GetPersonInfo(1, _knownPlayers.Select(x => x.Nickname).ToArray());
-            _knownPlayers.Add(_player1);
-
-            _player2 = GetPersonInfo(2, _knownPlayers.Select(x => x.Nickname).ToArray());
-            _knownPlayers.Add(_player2);
-
-            #endregion
-
             bool playAgain = false;
             do
             {
+                MenuBeginning();
                 PlayGame();
 
                 Console.WriteLine();
-
-
                 var menu = new ConsoleMenu<bool>("Wollt Ihr noch eine Runde spielen?", new List<ConsoleMenuItem>
                 {
                     new ConsoleMenuItem<bool>("Ja", x =>
                     {
                         playAgain = x;
+                        Console.Clear();
                     }, true),
                     new ConsoleMenuItem<bool>("Nein", x =>
                     {
@@ -48,47 +35,62 @@ namespace TicTacToeConsole
                     }, false)
                 });
                 menu.RunConsoleMenu();
-
-
             } while (playAgain);
+        }
+ 
+        private static void MenuBeginning()
+        {
+            var menuBeginning = new ConsoleMenu<string>("Gegen Wen willst du spielen?", new List<ConsoleMenuItem>
+                {
+                    new ConsoleMenuItem<string>("Versus HardBot", x =>
+                    {
+                        _player2 = new PerfectBot("Mahmoud [HardBot]");
+                    }),
+                    new ConsoleMenuItem<string>("Versus EasyBot", x =>
+                    {
+                        _player2 = new EasyBot("Robert [EasyBot]");
+                    }),
+                    new ConsoleMenuItem<string>("Versus Player2", x =>
+                    {
+                        if (_player2 != null && _player2.GetType() == typeof(HumanPlayer)) return;
+                        _player2 = GetPersonInfo(2, _knownPlayers.Select(x => x.Nickname).ToArray());
+                        _knownPlayers.Add(_player2);
+                    })
+                });
+            menuBeginning.RunConsoleMenu();
 
-
+            if (_player1 == null)
+            {
+                _player1 = GetPersonInfo(1, _knownPlayers.Select(x => x.Nickname).ToArray());
+                _knownPlayers.Add(_player1);
+            }
         }
 
         private static Player GetPersonInfo(int playerNumber, string[] notAllowed)
         {
-            var person = new Player
-            {
-                Nickname = InputHelper.GetUserText(
+            var person = new HumanPlayer(            
+                InputHelper.GetUserText(
                     $"Bitte Nickname für Spieler {playerNumber} eingeben",
                     "Sie haben ja gar nichts eingegeben.",
                     notAllowed,
                     "{{input}} ist bereits vergeben.",
-                    true)
-            };
+                    true));
             return person;
         }
 
-
         private static void PlayGame()
         {
-            Random random = new Random();
-            _game = new Game(_player1, _player2, _knownPlayers[random.Next(_knownPlayers.Count)]);
+            _game = new Game(_player1, _player2);
 
             while (!_game.GameEnd)
             {
                 Draw();
 
-                string inputx = Console.ReadLine();
-                string inputy = Console.ReadLine();
+                int inputx = InputHelper.GetUserInteger(null, "Sie haben ja gar nichts eingegeben.", 1, 3, "Bitte eine Zahl zwischen 1-3 eingeben.");
 
-                if (inputx == "3" || inputx == "2" || inputx == "1")
-                {
-                    if (inputy == "3" || inputy == "2" || inputy == "1")
-                    {
-                        _game.PlayerInput(Convert.ToInt32(inputx) - 1, Convert.ToInt32(inputy) - 1);
-                    }
-                }
+                int inputy = InputHelper.GetUserInteger(null, "Sie haben ja gar nichts eingegeben.", 1, 3, "Bitte eine Zahl zwischen 1-3 eingeben.");
+
+                _game.PlayerInput(inputx - 1,inputy - 1);
             }
 
             Draw();
@@ -140,7 +142,7 @@ namespace TicTacToeConsole
 
                     Console.SetCursorPosition(cursorIndex1, cursorIndexRows1);
 
-                    Console.Write($"{_game.Fields[i, j]}".Replace('1', 'X').Replace('2', 'O'));
+                    Console.Write($"{_game.Fields[j, i]}".Replace('1', 'X').Replace('2', 'O'));
                 }
                 cursorIndex1 = 0;
             }
@@ -151,26 +153,7 @@ namespace TicTacToeConsole
             #endregion
         }
 
-        private static bool GetYesNoDecsisionFromUser(string prompt, string failMessage)
-        {
-            bool firstLoop = true;
-
-            do
-            {
-                if (!firstLoop)
-                    Console.WriteLine(failMessage);
-
-                Console.Write($"{prompt} (j/n): ");
-                var input = Console.ReadKey();
-                switch (input.KeyChar)
-                {
-                    case 'j': return true;
-                    case 'n': return false;
-                }
-
-                firstLoop = true;
-            } while (true);
-
-        }
+        
     }
 }
+
